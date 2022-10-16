0b2d032e4621fe933f195737569a9ff916588008
saurav <sauravhell2000@gmail.com>
2022-10-16 08:29:46 +0545

    theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +12,101 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //final ThemeData theme = ThemeData();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Expenditure Tracker',
+      // theme: theme.copyWith(
+      //   colorScheme: theme.colorScheme.copyWith(
+      //     primary: Color.fromARGB(255, 20, 69, 22),
+      //     secondary: Color.fromARGB(255, 86, 118, 49),
+      //   ),
+      // ),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        accentColor: Color.fromARGB(255, 13, 67, 15),
+        // colorScheme: ColorScheme.dark(
+        //   secondary: Colors.red,
+        //   primary: Colors.white,
+        //   background: Colors.green,
+        // ),
+        // buttonTheme: ButtonThemeData(
+        //   buttonColor: Color.fromARGB(255, 86, 118, 49),
+        // ),
+        fontFamily: 'QuickSans',
+
+        // appBarTheme: AppBarTheme(
+        //   textTheme: ThemeData.light().textTheme.copyWith(
+        //         headline6: TextStyle(
+        //           fontFamily: 'Black',
+        //           fontWeight: FontWeight.w600,
+        //           fontSize: 22,
+        //         ),
+        //       ),
+        // ),
+
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontFamily: 'Larissa',
+                fontWeight: FontWeight.w600,
+                fontSize: 22,
+              ),
+            ),
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   // String titleInput = 'g';
-  // String amountInput = '0';
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 3000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Groceries',
+      amount: 900,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+          child: NewTransaction(_addNewTransaction),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +114,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Expenditure Tracker',
+          style: TextStyle(
+            fontFamily: 'Black',
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 30,
+          ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: const Icon(
+              Icons.add,
+            ),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -46,8 +146,15 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            UserTransaction(),
+            TransactionList(_userTransactions),
           ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _startAddNewTransaction(context),
+        child: const Icon(
+          Icons.add,
         ),
       ),
     );
